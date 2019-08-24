04e1065bde59c0ce1441cf502919115651c4268b
Philemon GLOBLEHI <philemongloblehi@gmail.com>
2019-08-24 19:26:56 +0000

    Initial commit

---
+++ src/main/java/com/api/fidelityms/FidelityMsApplication.java
@@ -0,0 +1,13 @@
+package com.api.fidelityms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidelityMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidelityMsApplication.class, args);
+	}
+
+}
